03a0ae4078657c591c91da346b8da924a24c8fe4
urvish <kaneriyaurvish@gmail.com>
2024-04-16 18:04:49 +0530

    search and filter

--- app/src/main/java/com/example/culinarycompass/HomeActivity.kt
+++ app/src/main/java/com/example/culinarycompass/HomeActivity.kt
@@ -5,25 +5,63 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
-import androidx.compose.material3.Button
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cancel
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.FilterList
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.sharp.Cancel
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.isTraversalGroup
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.traversalIndex
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
+import com.bumptech.glide.integration.compose.placeholder
 import com.example.culinarycompass.Viewmodels.HomeViewModel
 import com.example.culinarycompass.data.Hit
 import com.example.culinarycompass.data.Recipe
@@ -43,11 +81,9 @@ class HomeActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Column {
-
-
+                        Search()
                     displayList(viewModel = viewModel)
-                    Button(onClick = {viewModel.getrecipes("pastas")}) {
-                    }
+
                     }
                 }
             }
@@ -71,17 +107,73 @@ var data : State<MutableList<Hit>?> = viewModel.recepies.observeAsState()
         }
     }
 }
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun Search() {
+    var text by rememberSaveable { mutableStateOf("") }
+    Row (verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly){
 
+
+        SearchBar(
+            modifier = Modifier,
+            query = text,
+            onQueryChange = { text = it },
+            onSearch = {},
+            active = false,
+            onActiveChange = {},
+            placeholder = { Text("search recipies") },
+            leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
+            trailingIcon = {
+                if (text.isNotEmpty()) {
+                    IconButton(onClick = { text = "" }) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            tint = MaterialTheme.colorScheme.onSurface,
+                            contentDescription = "Clear search"
+                        )
+                    }
+                }
+            },
+        ) {}
+        Icon(Icons.Default.FilterList, contentDescription = null, modifier = Modifier.size(25.dp))
+    }
+}
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
-fun itemView(recipe: Recipe){
-    Column {
-        Text(text = recipe.label)
+fun itemView(recipe: Recipe) {
+    Box(modifier = Modifier
+        .padding(4.dp)
+        .clip(RoundedCornerShape(16.dp))) {
+
 GlideImage(model = recipe.image,
     contentDescription ="",
+    loading = placeholder( R.drawable.placeholder),
+    failure = placeholder(R.drawable.placeholder),
     contentScale = ContentScale.FillBounds,
-    modifier = Modifier.align(Alignment.CenterHorizontally).padding(4.dp))
 
-        Text(text = recipe.source.substringAfter("."))
+    modifier = Modifier
+        .size(200.dp)
+        .align(Alignment.Center)
+        .drawWithCache {
+            val gradient = Brush.verticalGradient(
+                colors = listOf(Color.Transparent, Color.Black),
+                startY = size.height / 3,
+                endY = size.height
+            )
+            onDrawWithContent {
+                drawContent()
+                drawRect(gradient, blendMode = BlendMode.Multiply)
+            }
+        }){
+       it
+
+}
+        Text(text = recipe.label, modifier = Modifier
+            .align(Alignment.BottomStart)
+            .padding(4.dp, 0.dp, 0.dp, 20.dp), color = Color.White, fontSize = 18.sp)
+
     }
+
 }
